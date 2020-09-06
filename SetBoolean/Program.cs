1670de389d3b1b7b78bb9220fea11c90d9adeeb7
Ivan Kornasevich <kornasevich@gmail.com>
2020-09-06 16:48:21 +0300

    to string is done

--- SetBoolean/Program.cs
+++ SetBoolean/Program.cs
@@ -1,29 +1,56 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace SetBoolean
 {
-    class Program
+    public static class Program
     {
-        static IEnumerable<IEnumerable<T>> PowerSet<T>(IEnumerable<T> initialSet)
+        public static StringBuilder ToStringAsSet(this IEnumerable set, int deep = 0)
+        {
+            var s = new StringBuilder();
+            var deeping = new string(Enumerable.Range(0, deep * 4).Select(x => ' ').ToArray());
+
+            foreach (var item in set)
+            {
+                if (item is IEnumerable col)
+                {
+                    s.Append("\n" + deeping + "[");
+                    s.Append(col.ToStringAsSet(deep + 1));
+                    s.Append("]\n");
+                }
+                else
+                {
+                    var it = set.GetEnumerator();
+
+                    while (it.MoveNext() == true)
+                    {
+                        s.Append(" " + it.Current + " ");
+                    }
+                    return s;
+                }
+            }
+            return s;
+        }
+
+        private static void Main()
+        {
+            var i = 6;
+            var set = Enumerable.Range(1, 3).Select(x => Enumerable.Range(i++, 2));
+            var powerset = PowerSet(set);
+
+            Console.WriteLine(powerset.ToStringAsSet());
+        }
+
+        private static IEnumerable<IEnumerable<T>> PowerSet<T>(IEnumerable<T> initialSet)
         {
             var boolean = new List<IEnumerable<T>>() { Enumerable.Empty<T>() } as IEnumerable<IEnumerable<T>>;
 
-            return initialSet.Aggregate(boolean, (b, x) => b.Concat(b.Select(z => z.Concat(new List<T>() { x }))));
-        }
-
-        static void Main()
-        {
-            var set = Enumerable.Range(1, 4);
-            var powerset = PowerSet(set);
-
-            foreach (var subset in powerset)
-            {
-                foreach (var element in subset)
-                    Console.Write("{0} ", element);
-                Console.WriteLine();
-            }
+            return initialSet.Aggregate(boolean, (b, x) => (b.Concat(b.Select(z => z.Concat(new List<T>() { x })))));
         }
     }
 }
---
+++ SetBoolean/Set.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SetBoolean
+{
+    internal class Set<T> : IEnumerable<T>
+    {
+        internal IEnumerable<T> Collection { get; private set; }
+
+        public Set(IEnumerable<T> set)
+        {
+            Collection = set;
+        }
+
+        internal Set<IEnumerable<T>> GetBoolean()
+        {
+            var boolean = new List<IEnumerable<T>>() { Enumerable.Empty<T>() } as IEnumerable<IEnumerable<T>>;
+
+            return new Set<IEnumerable<T>>(this.Aggregate(boolean, (b, x) => b.Concat(b.Select(z => z.Concat(new List<T>() { x })))));
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Collection.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
