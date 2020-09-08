cc1b028b96052b88809fb6fa1114351a7fb2d390
Ivan Kornasevich <kornasevich@gmail.com>
2020-09-08 22:03:33 +0300

    GetBoolean

--- SetBoolean/Program.cs
+++ SetBoolean/Program.cs
@@ -10,47 +10,11 @@ namespace SetBoolean
 {
     public static class Program
     {
-        public static StringBuilder ToStringAsSet(this IEnumerable set, int deep = 0)
-        {
-            var s = new StringBuilder();
-            var deeping = new string(Enumerable.Range(0, deep * 4).Select(x => ' ').ToArray());
-
-            foreach (var item in set)
-            {
-                if (item is IEnumerable col)
-                {
-                    s.Append("\n" + deeping + "[");
-                    s.Append(col.ToStringAsSet(deep + 1));
-                    s.Append("]\n");
-                }
-                else
-                {
-                    var it = set.GetEnumerator();
-
-                    while (it.MoveNext() == true)
-                    {
-                        s.Append(" " + it.Current + " ");
-                    }
-                    return s;
-                }
-            }
-            return s;
-        }
-
         private static void Main()
         {
-            var i = 6;
-            var set = Enumerable.Range(1, 3).Select(x => Enumerable.Range(i++, 2));
-            var powerset = PowerSet(set);
+            Console.WriteLine(new Set<int>(Enumerable.Range(1, 3)).GetBoolean());
 
-            Console.WriteLine(powerset.ToStringAsSet());
-        }
-
-        private static IEnumerable<IEnumerable<T>> PowerSet<T>(IEnumerable<T> initialSet)
-        {
-            var boolean = new List<IEnumerable<T>>() { Enumerable.Empty<T>() } as IEnumerable<IEnumerable<T>>;
-
-            return initialSet.Aggregate(boolean, (b, x) => (b.Concat(b.Select(z => z.Concat(new List<T>() { x })))));
+            //Console.WriteLine(set.GetBoolean().ToString());
         }
     }
 }
--- SetBoolean/Set.cs
+++ SetBoolean/Set.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 
@@ -7,18 +9,75 @@ namespace SetBoolean
 {
     internal class Set<T> : IEnumerable<T>
     {
-        internal IEnumerable<T> Collection { get; private set; }
+        internal List<T> Collection { get; private set; }
+
+        public Set()
+        {
+            Collection = new List<T>();
+        }
 
         public Set(IEnumerable<T> set)
         {
-            Collection = set;
+            Collection = set.ToList();
         }
 
-        internal Set<IEnumerable<T>> GetBoolean()
+        internal void Add(T item)
         {
-            var boolean = new List<IEnumerable<T>>() { Enumerable.Empty<T>() } as IEnumerable<IEnumerable<T>>;
+            Collection.Add(item);
+        }
 
-            return new Set<IEnumerable<T>>(this.Aggregate(boolean, (b, x) => b.Concat(b.Select(z => z.Concat(new List<T>() { x })))));
+        internal Set<Set<T>> GetBoolean()
+        {
+            Set<T> GetBooleanSubSet(int bitMask)
+            {
+                var subSet = new Set<T>();
+
+                for (var collIdx = 0; (collIdx < Collection.Count) && (bitMask > 0); collIdx++)
+                {
+                    if ((bitMask & 1) == 1)
+                    {
+                        subSet.Collection.Add(Collection[collIdx]);
+                    }
+                    bitMask >>= 1;
+                }
+                return subSet;
+            }
+
+            var boolean = new Set<T>[(int)Math.Pow(2, Collection.Count)];
+
+            for (int bitMask = 0; bitMask < boolean.Length; ++bitMask)
+            {
+                boolean[bitMask] = GetBooleanSubSet(bitMask) as Set<T>;
+            }
+            return new Set<Set<T>>(boolean);
+        }
+
+        internal Set<Set<T>> GetBooleanGray()
+        {
+            var boolean = new Set<Set<T>>() { new Set<T>(Enumerable.Empty<T>()) };
+
+            return this.Aggregate(boolean, (b, x) => new Set<Set<T>>(b.Concat(b.Select(z => z.Concat(new Set<T>() { x }))).Select(x => new Set<T>(x))));
+        }
+
+        public override string ToString()
+        {
+            var s = new StringBuilder();
+            s.Append('{');
+            foreach (var item in Collection)
+            {
+                if (item is IEnumerable coll)
+                {
+                    s.Append(string.Join(",\n", coll));
+                }
+                else
+                {
+                    s.Append(string.Join(", ", Collection));
+
+                    break;
+                }
+            }
+            s.Append("}\n");
+            return s.ToString();
         }
 
         public IEnumerator<T> GetEnumerator()
